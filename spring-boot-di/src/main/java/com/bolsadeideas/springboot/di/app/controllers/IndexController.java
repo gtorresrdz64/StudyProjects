124f7bfab0874ac89f5d05cdb4948be04292868a
Gustavo Torres R <gtorres5@mmm.com>
2019-09-18 14:13:46 -0600

    New project Dependency Inyection}

---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.bolsadeideas.springboot.di.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bolsadeideas.springboot.di.app.models.services.MiServicio;
+
+@Controller
+public class IndexController {
+
+	private MiServicio servicio= new MiServicio();
+	
+	@GetMapping({"/","","index"})
+	public String index(Model model) {
+		
+		model.addAttribute("objecto",servicio.operacion());
+		return "index";
+	}
+}
---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,10 @@
+package com.bolsadeideas.springboot.di.app.models.services;
+
+public class MiServicio {
+
+	public String operacion() {
+		
+		return "ejecutando algun proceso importante";
+		
+	}
+}
