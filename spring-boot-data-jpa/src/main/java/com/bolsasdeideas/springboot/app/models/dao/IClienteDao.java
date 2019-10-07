3ad1d511963dbc8e10ec4aeebe62f08f9c1f687a
Gustavo <gtorresrodrig64@gmail.com>
2019-10-06 21:46:45 -0600

    Class 62

--- spring-boot-data-jpa/src/main/java/com/bolsasdeideas/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/bolsasdeideas/springboot/app/models/dao/IClienteDao.java
@@ -1,12 +1,19 @@
 package com.bolsasdeideas.springboot.app.models.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import com.bolsasdeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
 	
-	public List<Cliente> findAll();
+	
+	
+	
+	/*public List<Cliente> findAll();
 	
 	public void save(Cliente cliente);
+	
+	public Cliente findOne(Long id); 
+	
+	public void delete(Long id);*/
 }
---
+++ spring-boot-data-jpa/src/main/java/com/bolsasdeideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -0,0 +1,46 @@
+package com.bolsasdeideas.springboot.app.models.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsasdeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsasdeideas.springboot.app.models.entity.Cliente;
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+
+	
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public void save(Cliente cliente) {
+		clienteDao.save(cliente);
+		
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findOne(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+		
+	}
+
+}
