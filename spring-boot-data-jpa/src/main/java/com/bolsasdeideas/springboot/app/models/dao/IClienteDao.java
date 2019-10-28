af0404fa9a4980aec01abfe0ab94fe380013bad8
Gustavo <gtorresrodrig64@gmail.com>
2019-10-27 22:03:27 -0600

    Pagination Video3

--- spring-boot-data-jpa/src/main/java/com/bolsasdeideas/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/bolsasdeideas/springboot/app/models/dao/IClienteDao.java
@@ -1,10 +1,11 @@
 package com.bolsasdeideas.springboot.app.models.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.bolsasdeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long>{
 	
 	
 	
--- spring-boot-data-jpa/src/main/resources/import.sql
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -1,2 +1,30 @@
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Gustavo', 'Torres', 'gtorresrodrig64@gmail.com','2019-10-2');
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3, 'Cliente3', 'Tercero', 'Cliente3@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (4, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (5, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (6, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (7, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (8, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (9, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (10, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (11, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (12, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (13, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (14, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (15, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (16, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (17, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (18, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (19, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (20, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (21, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (22, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (23, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (24, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (25, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (26, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (27, 'Cliente2', 'Segundo', 'Cliente2@gmail.com','2019-10-2');
+
+
+
